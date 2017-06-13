481eb89df116738af7cfa74eec8a488a29380ea5
Luke Wilson <luke@broughtbackbooks.com>
2017-06-13 09:01:05 -0500

    Add component array

--- index.js
+++ index.js
@@ -1,13 +1,112 @@
 const alfy = require('alfy');
 
-alfy.fetch('jsonplaceholder.typicode.com/posts').then(data => {
-	const items = alfy
-		.inputMatches(data, 'title')
-		.map(x => ({
-			title: x.title,
-			subtitle: x.body,
-			arg: x.id
-		}));
-
-	alfy.output(items);
-});
+ const components = [
+    {
+      "name": "Anatomy",
+      "url": "https://docs.nativebase.io/Components.html#anatomy-headref"
+    },
+    {
+      "name": "ActionSheet",
+      "url": "https://docs.nativebase.io/Components.html#ActionSheet"
+    },
+    {
+      "name": "Badge",
+      "url": "https://docs.nativebase.io/Components.html#Badge"
+    },
+    {
+      "name": "Button",
+      "url": "https://docs.nativebase.io/Components.html#button-headref"
+    },
+    {
+      "name": "Card",
+      "url": "https://docs.nativebase.io/Components.html#card-def-headref"
+    },
+    {
+      "name": "Check Box",
+      "url": "https://docs.nativebase.io/Components.html#checkbox-headref"
+    },
+    {
+      "name": "Deck Swiper",
+      "url": "https://docs.nativebase.io/Components.html#deckswiper-headref"
+    },
+    {
+      "name": "FABs",
+      "url": "https://docs.nativebase.io/Components.html#fabs-def-headref"
+    },
+    {
+      "name": "Muliple Fabs",
+      "url": "https://docs.nativebase.io/Components.html#fabs-multiple-headref"
+    },
+    {
+      "name": "Footer Tabs",
+      "url": "https://docs.nativebase.io/Components.html#footer-tabs-headref"
+    },
+    {
+      "name": "Form",
+      "url": "https://docs.nativebase.io/Components.html#Form"
+    },
+    {
+      "name": "Header",
+      "url": "https://docs.nativebase.io/Components.html#header-def-headref"
+    },
+    {
+      "name": "Icon",
+      "url": "https://docs.nativebase.io/Components.html#icon-def-headref"
+    },
+    {
+      "name": "Layout",
+      "url": "https://docs.nativebase.io/Components.html#Layout"
+    },
+    {
+      "name": "List",
+      "url": "https://docs.nativebase.io/Components.html#list-def-headref"
+    },
+    {
+      "name": "Picker",
+      "url": "https://docs.nativebase.io/Components.html#Picker"
+    },
+    {
+      "name": "Segment",
+      "url": "https://docs.nativebase.io/Components.html#Segment"
+    },
+    {
+      "name": "Radio Button",
+      "url": "https://docs.nativebase.io/Components.html#radio-button-headref"
+    },
+    {
+      "name": "Search Bar",
+      "url": "https://docs.nativebase.io/Components.html#search-bar-headref"
+    },
+    {
+      "name": "Spinner",
+      "url": "https://docs.nativebase.io/Components.html#Spinner"
+    },
+    {
+      "name": "Tabs",
+      "url": "https://docs.nativebase.io/Components.html#tabs-def-headref"
+    },
+    {
+      "name": "Advanced Tabs",
+      "url": "https://docs.nativebase.io/Components.html#advanced-tabs-headref"
+    },
+    {
+      "name": "Thumbnail",
+      "url": "https://docs.nativebase.io/Components.html#Thumbnail"
+    },
+    {
+      "name": "Toast",
+      "url": "https://docs.nativebase.io/Components.html#Toast"
+    },
+    {
+      "name": "Typography",
+      "url": "https://docs.nativebase.io/Components.html#Typography"
+    },
+    {
+      "name": "Drawer",
+      "url": "https://docs.nativebase.io/Components.html#Drawer"
+    },
+    {
+      "name": "Ref",
+      "url": "https://docs.nativebase.io/Components.html#ref-components-headref"
+    }
+]
