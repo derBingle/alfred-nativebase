e92bf8bc6f1243fa96be6275f2169b83ed109f53
Luke Wilson <luke@broughtbackbooks.com>
2017-06-13 09:24:37 -0500

    Add alfy matching and output

--- index.js
+++ index.js
@@ -1,112 +1,41 @@
 const alfy = require('alfy');
 
- const components = [
-    {
-      "name": "Anatomy",
-      "url": "https://docs.nativebase.io/Components.html#anatomy-headref"
-    },
-    {
-      "name": "ActionSheet",
-      "url": "https://docs.nativebase.io/Components.html#ActionSheet"
-    },
-    {
-      "name": "Badge",
-      "url": "https://docs.nativebase.io/Components.html#Badge"
-    },
-    {
-      "name": "Button",
-      "url": "https://docs.nativebase.io/Components.html#button-headref"
-    },
-    {
-      "name": "Card",
-      "url": "https://docs.nativebase.io/Components.html#card-def-headref"
-    },
-    {
-      "name": "Check Box",
-      "url": "https://docs.nativebase.io/Components.html#checkbox-headref"
-    },
-    {
-      "name": "Deck Swiper",
-      "url": "https://docs.nativebase.io/Components.html#deckswiper-headref"
-    },
-    {
-      "name": "FABs",
-      "url": "https://docs.nativebase.io/Components.html#fabs-def-headref"
-    },
-    {
-      "name": "Muliple Fabs",
-      "url": "https://docs.nativebase.io/Components.html#fabs-multiple-headref"
-    },
-    {
-      "name": "Footer Tabs",
-      "url": "https://docs.nativebase.io/Components.html#footer-tabs-headref"
-    },
-    {
-      "name": "Form",
-      "url": "https://docs.nativebase.io/Components.html#Form"
-    },
-    {
-      "name": "Header",
-      "url": "https://docs.nativebase.io/Components.html#header-def-headref"
-    },
-    {
-      "name": "Icon",
-      "url": "https://docs.nativebase.io/Components.html#icon-def-headref"
-    },
-    {
-      "name": "Layout",
-      "url": "https://docs.nativebase.io/Components.html#Layout"
-    },
-    {
-      "name": "List",
-      "url": "https://docs.nativebase.io/Components.html#list-def-headref"
-    },
-    {
-      "name": "Picker",
-      "url": "https://docs.nativebase.io/Components.html#Picker"
-    },
-    {
-      "name": "Segment",
-      "url": "https://docs.nativebase.io/Components.html#Segment"
-    },
-    {
-      "name": "Radio Button",
-      "url": "https://docs.nativebase.io/Components.html#radio-button-headref"
-    },
-    {
-      "name": "Search Bar",
-      "url": "https://docs.nativebase.io/Components.html#search-bar-headref"
-    },
-    {
-      "name": "Spinner",
-      "url": "https://docs.nativebase.io/Components.html#Spinner"
-    },
-    {
-      "name": "Tabs",
-      "url": "https://docs.nativebase.io/Components.html#tabs-def-headref"
-    },
-    {
-      "name": "Advanced Tabs",
-      "url": "https://docs.nativebase.io/Components.html#advanced-tabs-headref"
-    },
-    {
-      "name": "Thumbnail",
-      "url": "https://docs.nativebase.io/Components.html#Thumbnail"
-    },
-    {
-      "name": "Toast",
-      "url": "https://docs.nativebase.io/Components.html#Toast"
-    },
-    {
-      "name": "Typography",
-      "url": "https://docs.nativebase.io/Components.html#Typography"
-    },
-    {
-      "name": "Drawer",
-      "url": "https://docs.nativebase.io/Components.html#Drawer"
-    },
-    {
-      "name": "Ref",
-      "url": "https://docs.nativebase.io/Components.html#ref-components-headref"
-    }
+const components = [
+  { name: "ActionSheet",   hash: "ActionSheet" },
+  { name: "Advanced Tabs", hash: "advanced-tabs-headref" },
+  { name: "Anatomy",       hash: "anatomy-headref" },
+  { name: "Badge",         hash: "Badge" },
+  { name: "Button",        hash: "button-headref" },
+  { name: "Card",          hash: "card-def-headref" },
+  { name: "Check Box",     hash: "checkbox-headref" },
+  { name: "Deck Swiper",   hash: "deckswiper-headref" },
+  { name: "Drawer",        hash: "Drawer" },
+  { name: "FABs",          hash: "fabs-def-headref" },
+  { name: "Footer Tabs",   hash: "footer-tabs-headref" },
+  { name: "Form",          hash: "Form" },
+  { name: "Header",        hash: "header-def-headref" },
+  { name: "Icon",          hash: "icon-def-headref" },
+  { name: "Layout",        hash: "Layout" },
+  { name: "List",          hash: "list-def-headref" },
+  { name: "Muliple Fabs",  hash: "fabs-multiple-headref" },
+  { name: "Picker",        hash: "Picker" },
+  { name: "Radio Button",  hash: "radio-button-headref" },
+  { name: "Ref",           hash: "ref-components-headref" },
+  { name: "Search Bar",    hash: "search-bar-headref" },
+  { name: "Segment",       hash: "Segment" },
+  { name: "Spinner",       hash: "Spinner" },
+  { name: "Tabs",          hash: "tabs-def-headref" },
+  { name: "Thumbnail",     hash: "Thumbnail" },
+  { name: "Toast",         hash: "Toast" },
+  { name: "Typography",    hash: "Typography" },
 ]
+
+const items = alfy.inputMatches(components, 'name')
+              .map(component => ({
+                    uuid: component.name.toLowerCase,
+			              title: component.name.toLowerCase,
+		              	subtitle: `Open NativeBase docs for ${ component.name }`,
+		              	arg: component.hash
+          		})) 
+
+alfy.output(items)
